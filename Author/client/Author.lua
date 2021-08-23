4eea4ce9c0b6dc088f05056e562263bad5f609fb
Hallkezz <32846474+Hallkezz@users.noreply.github.com>
2021-08-23 19:58:17 +0300

    Add files via upload

---
+++ Author/client/Author.lua
@@ -0,0 +1,24 @@
+class 'Author'
+
+function Author:__init()
+    Events:Subscribe( "ModulesLoad", self, self.ModulesLoad )
+    Events:Subscribe( "ModuleUnload", self, self.ModuleUnload )
+end
+
+function Author:ModulesLoad()
+    Events:Fire( "HelpAddItem",
+        {
+            name = "Локализация",
+            text = 
+                "Автор: Hallkezz\n"
+        } )
+end
+
+function Author:ModuleUnload()
+    Events:Fire( "HelpRemoveItem",
+        {
+            name = "Локализация"
+        } )
+end
+
+author = Author()
